06176db571901241d5e0694e65736cc2d42334b1
Emmanuel Valenzuela <ing.valen0421@hotmail.com>
2021-12-23 00:22:05 -0400

    finalize the init structure of the project 

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card.js'
 
 const App = () => {
   return (
     <div className='container mx-auto'>
-        <h1>Tailwind CSS</h1>
-        <h2>Incorporado con PostCss</h2>
-        <button className="btn btn-blue btn-blue:hover">Botón</button>
+        <h1>Pokedex + React + TailwindCSS</h1>
+        {/* <button className="btn btn-blue btn-blue:hover">Botón</button> */}
         <div className="my-5 flex flex-wrap -mx-2">
             <div className="w-full sm:w-1/2 lg:w-1/3 px-2 my-2">
                 <Card photo='https://picsum.photos/800/600' />
